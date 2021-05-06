05bcd5532671752508674261ed94028befb5d595
Sarah dePalo <sarahdepalo99@gmail.com>
2021-05-06 12:22:48 -0400

    added loop1.py

---
+++ loop1.py
@@ -0,0 +1,9 @@
+title = "Harry Potter & the Half-Blood Prince"
+# STOP = 10
+counter = 0
+
+while counter < len(title): 
+    if (counter % 2) == 0:
+        print(title[counter])
+    counter += 1
+    
